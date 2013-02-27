42b98fea31017e219c8391b37ad3f5bdf569a12d
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2013-02-27 15:49:46 -0500

    Initial commit.

---
+++ composer_manager.api.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Composer module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Allow modules to alter the json data in the composer file.
+ *
+ * @param array &$data
+ *   The array that will be converted to JSON, which is the contents of the
+ *   composer.json file.
+ */
+function hook_composer_manager_json_alter(&$data) {
+  $data['minimum-stability'] = 'dev';
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
